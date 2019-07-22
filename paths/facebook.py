d2418010b7cc0b5b3f3ad7fe23309d996c92bb0a
Nicolas Breinl <nicolas.breinl@gmail.com>
2019-07-22 14:52:05 +0200

    New paths

--- paths/facebook.py
+++ paths/facebook.py
@@ -11,3 +11,6 @@ SECOND_SUBMIT_XPATH = '//*[@id="u_0_a"]'
 THIRD_SUBMIT_XAPTH = '//*[@id="u_0_2"]'
 NOTIFICATIONS_XPATH = '//*[@id="fbNotificationsJewel"]'
 ALL_NOTIFICATIONS_XPATH = '//*[@id="fbNotificationsFlyout"]/div[4]/a/span'
+
+# //*[@id="u_fetchstream_3_0"]/div/ul/li[1]/div/div/a/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/span
+# //*[@id="u_fetchstream_3_0"]/div/ul/li[2]/div/div/a/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/span
